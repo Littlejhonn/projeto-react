a0cb2dd24b0a9b56f2d26c802c3d32fe346aeeaf
Littlejhonn <joaocba16@gmail.com>
2025-04-24 22:40:26 -0300

    Adicionei estilo a 2 componentes

---
+++ aula1-react/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Saudacao from './components/Saudacao'
+import Formulario from './components/Formulario'
+
+function App() {
+  return(
+    <>
+    
+    <Saudacao nome="João" sobrenome="Carlos"/>
+    <Formulario/>
+    </>
+  )
+}
+  
+
+export default App
---
+++ aula1-react/src/components/Saudacao/index.jsx
@@ -0,0 +1,8 @@
+import './style.css';
+
+
+function Saudacao({nome, sobrenome}) {
+    return <p>Olá, {nome} {sobrenome}! Seja bem vindo!</p>
+}
+
+export default Saudacao;
